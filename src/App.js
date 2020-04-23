c315bbc1bd5171ed8dba69edadf4492641cba9d4
Anneloes <anneloes9405@hotmail.com>
2020-04-23 15:47:54 -0400

    added Footer, made some fixes all over
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
