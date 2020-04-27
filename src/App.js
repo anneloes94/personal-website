de3af65e1b877e85bc604babcd68fb35b928ce97
Anneloes <anneloes9405@hotmail.com>
2020-04-27 13:40:45 -0400

    page now has a preloader - need to alter colours
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
+      <div id="preloader"> 
+        <div id="loader"></div>
+      </div>
       <Navigation />
       <Home />
       <About />
